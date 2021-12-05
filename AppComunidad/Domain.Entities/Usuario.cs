e3e1bc5ef2e0ccf4bbac4b4b8bf4e58e8a7a9906
Jose Luis Martinez <lmartinez@csticorp.biz>
2021-12-05 03:37:55 -0500

    COMMIT

---
+++ AppComunidad/Domain.Entities/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Usuario : Entity
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int TipoDocumentoId { get; set; }
+        public string NroDocumento { get; set; }
+        public int TipoUsuarioId { get; set; }
+
+    }
+}
